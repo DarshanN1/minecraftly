d5df224222fcd3e1252e70494780a2c18d097011
minecraftly <minecraftly@users.noreply.github.com>
2016-03-16 23:45:36 +0700

    Update and rename quick-install-debian.sh to auto-install-debian.sh

--- auto-install-debian.sh
+++ auto-install-debian.sh
@@ -6,6 +6,7 @@ apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 apt-get upgrade -y
 apt-get dist-upgrade -y
+apt-get install screen -y
 
 #Change ulimit
 ulimit -n 1048576
@@ -26,8 +27,16 @@ apt-get install oracle-java8-set-default -y
 mkdir /minecraftly
 mkdir /minecraftly/bungeecord1
 mkdir /minecraftly/bungeecord2
+mkdir /minecraftly/buildtools
 mkdir /minecraftly/spigot1
 mkdir /minecraftly/spigot2
+
+#Download some files
+bash -c "wget -P /minecraftly/bungeecord1 http://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar"
+bash -c "wget -P /minecraftly/bungeecord2 http://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar"
+bash -c "wget -P /minecraftly/buildtools https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar && cd /minecraftly/buildtools && java -jar BuildTools.jar"
+cp /minecraftly/buildtools/spigot-1.9.jar /minecraftly/spigot1/spigot-1.9.jar
+cp /minecraftly/buildtools/spigot-1.9.jar /minecraftly/spigot2/spigot-1.9.jar
 
 #Configure some stuffs
 sed -i "s/server-id:.*/server-id: localhost/" /minecraftly/bungeecord1/plugins/RedisBungee/config.yml

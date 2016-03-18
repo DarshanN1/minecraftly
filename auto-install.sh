71448a6ccf924b9fa1b14f6e0bba4b588570dbd4
Viet Nguyen <viet@users.noreply.github.com>
2016-03-18 21:47:35 +0700

    Update and rename auto-install-debian.sh to auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -1,4 +1,5 @@
-#This is a script to automatically install Minecraftly on one Debian server
+#This is a script to automatically install Minecraftly on a single server for testing
+#Requirements: Debian 8 or higher. Server needs at least 2GB RAM and 1 virtual CPU
 #! /bin/bash
 #Update some stuffs
 sudo -i

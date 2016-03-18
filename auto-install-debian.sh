1eb60d210300c5f214cb69ef043272f9f27dd418
Viet Nguyen <viet@users.noreply.github.com>
2016-03-18 21:27:39 +0700

    Update auto-install-debian.sh

--- auto-install-debian.sh
+++ auto-install-debian.sh
@@ -15,8 +15,11 @@ apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
 add-apt-repository 'deb http://mariadb.biz.net.id//repo/10.1/debian sid main'
 apt-get install software-properties-common -y
 apt-get update -y
+export DEBIAN_FRONTEND=noninteractive
+sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password 123456'
+sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password 123456'
+mysql -uroot -pPASS -e "SET PASSWORD = PASSWORD('');"
 sudo -E apt-get install mariadb-server mariadb-client -q -y
-mysqladmin -u root password 123456
 
 #Install Redis
 apt-get install build-essential -y

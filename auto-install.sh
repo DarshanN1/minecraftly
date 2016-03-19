76be9406f784dfdd68ce28b8965db971c87963de
Viet Nguyen <viet@users.noreply.github.com>
2016-03-19 14:41:53 +0700

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -70,6 +70,12 @@ cd /minecraftly/spigot2 && screen -dmS spigot2 java -Dcom.mojang.eula.agree=true
 sleep 30
 screen -r spigot2 -X stuff 'stop\n'
 
+#Download Minecraftly plugins
+wget -P /minecraftly/bungeecord1/plugins https://ci.m.ly/job/Minecraftly/lastSuccessfulBuild/artifact/target/MinecraftlyBungee.jar
+wget -P /minecraftly/bungeecord2/plugins https://ci.m.ly/job/Minecraftly/lastSuccessfulBuild/artifact/target/MinecraftlyBungee.jar
+wget -P /minecraftly/spigot1/plugins https://ci.m.ly/job/Minecraftly/lastSuccessfulBuild/artifact/target/MinecraftlySpigot.jar
+wget -P /minecraftly/spigot2/plugins https://ci.m.ly/job/Minecraftly/lastSuccessfulBuild/artifact/target/MinecraftlySpigot.jar
+
 #Start servers for the second time to generate plugin files
 cd /minecraftly/bungeecord1 && screen -dmS bungeecord1 java -jar BungeeCord.jar
 sleep 30

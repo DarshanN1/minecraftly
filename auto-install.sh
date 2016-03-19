0368314dc84c77c501e4858dc327d76bc9fdc493
Viet Nguyen <viet@users.noreply.github.com>
2016-03-19 20:59:35 +0700

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -91,8 +91,8 @@ wget -P /minecraftly/bungeecord1/plugins https://m.ly/MinecraftlyBungee.jar
 wget -P /minecraftly/bungeecord2/plugins https://m.ly/MinecraftlyBungee.jar
 wget -P /minecraftly/spigot1/plugins https://m.ly/MinecraftlySpigot.jar
 wget -P /minecraftly/spigot2/plugins https://m.ly/MinecraftlySpigot.jar
-wget -P /minecraftly/spigot1/plugins https://m.ly/ProtocolLib.jar
-wget -P /minecraftly/spigot2/plugins https://m.ly/ProtocolLib.jar
+wget -P /minecraftly/spigot1/plugins http://dev.bukkit.org/media/files/894/359/Vault.jar
+wget -P /minecraftly/spigot2/plugins http://dev.bukkit.org/media/files/894/359/Vault.jar
 
 #Start servers for the second time to generate Minecraftly plugin files
 cd /minecraftly/bungeecord1 && screen -dmS bungeecord1 java -jar BungeeCord.jar
